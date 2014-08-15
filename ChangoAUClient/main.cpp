14fc68496b578738a88fc4dec22d279c0df14a6c
Brandon Lucia <blucia@gmail.com>
2014-08-14 19:28:10 -0700

    Initial Commit

---
+++ ChangoAUClient/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ChangoAUClient
+//
+//  Created by Brandon Lucia on 8/14/14.
+//  Copyright (c) 2014 Brandon Lucia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
